9ad073e8351a82d4d1ae50b23976d032fad0ef6b
ZedBee27 <bc210202550@vu.edu.pk>
2025-05-06 18:51:00 +0500

    Search function in dal

--- src/components/web/homepage/ProductCarousel.tsx
+++ src/components/web/homepage/ProductCarousel.tsx
@@ -1,11 +1,8 @@
-import { getAllProducts, getProductsByCategory } from '@/lib/dal'
+import { getAllProducts } from '@/lib/dal'
 import ProductCard2 from './ProductCard2'
 
 export const ProductCarousel = async () => {
   const products = await getAllProducts()
-  const prod = await getProductsByCategory('alarms')
-  console.log(prod)
-
   return (
     <div className="w-full py-8">
       <h1 className="mb-6 px-4 text-2xl font-bold">Featured Products</h1>
